f4c43c2b6b53649ac038e07e0cf34db12222127e
Ashwarya <kaishwaryaramesh@gmail.com>
2020-09-21 22:17:00 +0530

    addressed review comments

--- code/sandbox/aishwarya/readYamlFile/readYaml.py
+++ code/sandbox/aishwarya/readYamlFile/readYaml.py
@@ -1,17 +1,30 @@
+import os
 import yaml
-
-with open('parameters.yaml') as f:
-    dataMap = yaml.safe_load(f)
 
 
 class Command:
-    def __init__(self, args):
-        self.__dict__.update(args)
+    def __init__(self, resource, action, parameters):
+        self.resource = resource
+        self.action = action
+        self.parameters = parameters
 
 
-s = Command(dataMap)
-print(s.resource)
-print(s.action)
-print(s.parameters)
-print(s.nested)
-print(s.users)
+class FileLoader:
+    def __init__(self, file_name):
+        self.file_name = file_name
+
+    def load_file(self):
+        with open(self.file_name) as f:
+            data_map = yaml.safe_load(f)
+            command = Command(data_map['resource'], data_map['action'], data_map['parameters'])
+
+        return command
+
+
+user_input = input("Enter the path of your file: ")
+assert os.path.exists(user_input), "I did not find the file at, "+str(user_input)
+file = FileLoader(user_input)
+yaml_object = file.load_file()
+print(yaml_object.resource)
+print(yaml_object.action)
+print(yaml_object.parameters)
