f03ebd2476d6768c2420717182a954ff7b5ce2da
Verma,Shubhaangi <Shubhaangi.Verma@cerner.com>
2020-10-10 19:10:37 +0530

    Added click grouping command

--- code/sandbox/kv/magic-user-interface/input_manager/command_input_manager.py
+++ code/sandbox/kv/magic-user-interface/input_manager/command_input_manager.py
@@ -1,10 +1,21 @@
-def command_1():
-    pass
+import click
 
 
-def command_2():
-    pass
-
-
+@click.group(invoke_without_command=True)
 def init_all_commands():
     pass
+
+
+@init_all_commands.command()
+def command_1():
+    click.echo("Printing 1")
+    pass
+
+
+@init_all_commands.command()
+def command_2():
+    click.echo("Printing 2")
+    pass
+
+
+
