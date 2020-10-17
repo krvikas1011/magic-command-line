98c1fa8029229e7717edc046abfbce7f86ffa235
Verma,Shubhaangi <Shubhaangi.Verma@cerner.com>
2020-10-17 21:18:47 +0530

    Added function for each resource

--- code/sandbox/kv/magic-user-interface/input_manager/command_input_manager.py
+++ code/sandbox/kv/magic-user-interface/input_manager/command_input_manager.py
@@ -7,14 +7,52 @@ def init_all_commands():
 
 
 @init_all_commands.command()
-def command_1():
-    click.echo("Printing 1")
+def file():
     pass
 
 
 @init_all_commands.command()
-def command_2():
-    click.echo("Printing 2")
+def folder():
+    pass
+
+
+@init_all_commands.command()
+def directory():
+    pass
+
+
+@init_all_commands.command()
+def software():
+    pass
+
+
+@init_all_commands.command()
+def ip():
+    pass
+
+
+@init_all_commands.command()
+def ping():
+    pass
+
+
+@init_all_commands.command()
+def process():
+    pass
+
+
+@init_all_commands.command()
+def system():
+    pass
+
+
+@init_all_commands.command()
+def disk():
+    pass
+
+
+@init_all_commands.command()
+def terminal():
     pass
 
 
